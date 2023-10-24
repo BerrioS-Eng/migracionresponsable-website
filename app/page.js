ba88363903e788e6a009fe6b2e81eeb9b62f76a3
Sebastian Berrio <sberrio09@gmail.com>
2023-10-24 02:08:59 -0500

    feature 01: first-section

--- app/page.js
+++ app/page.js
@@ -1,95 +1,73 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import bg from '@/public/background0.jpg'
+import logo from '@/public/logo.png'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className={styles.main}
+      style={{
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.67), rgba(0, 0, 0, 0.67)), url(${bg.src})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+      }}>
+      <div className={styles.header}>
+        <Image
+          className={styles.logo}
+          src={logo.src}
+          alt='Logo Migracion Responsable'
+          width={160}
+          height={120}
+          priority
+        />
+        <div className={styles.contentHeaderUpper}>
+          <h1>¡Tu viaje hacia una vida llena de oportunidades y desafíos comienza aquí!</h1>
+          <div className={styles.contentHeaderLower}>
+            <div>
+              <p>No estas solo, estamos aquí para ser tus compañeros de ruta, tus guías, y tus aliados en cada paso del proceso de migración.</p>
+              <div className={styles.socialmedia}>
+                <Link href="/">
+                  <img width="48"
+                    height="48"
+                    src="https://img.icons8.com/color/48/facebook-new.png"
+                    alt="facebook-new" />
+                </Link>
+                <Link href="/">
+                  <img width="48"
+                    height="48"
+                    src="https://img.icons8.com/color/96/instagram-new--v1.png"
+                    alt="instagram-new--v1" />
+                </Link>
+                <Link href="/">
+                  <img width="48"
+                    height="48"
+                    src="https://img.icons8.com/color/48/tiktok--v1.png"
+                    alt="tiktok--v1" />
+                </Link>
+                <Link href="/">
+                  <img width="48"
+                    height="48"
+                    src="https://img.icons8.com/color/48/twitterx--v1.png"
+                    alt="twitterx--v1" />
+                </Link>
+              </div>
+              <Link href="https://calendly.com/migracionresponsable/asesoria-migracion?month=2023-10"
+                className={styles.makeAppointment}>
+                Programa una cita con nuestro asesor</Link>
+            </div>
+            <iframe width="480" height="280" src="https://www.youtube.com/embed/OeZPQid1UE8?si=rLfz40_CBiFS8BiG" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+          </div>
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className={styles.content}>
+
       </div>
+      <div className={styles.footer}>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
+
   )
 }
